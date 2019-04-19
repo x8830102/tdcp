2cff1c16fb238b315864f8ff7e997e3d3e9a337c
pa_ku <paku@panmedia.asia>
2019-04-19 13:13:29 +0800

    single page 排版

--- wp-content/themes/alterna/inc/custom-function.php
+++ wp-content/themes/alterna/inc/custom-function.php
@@ -12,14 +12,14 @@ function get_post_list_func( $term_name, $posts_per_page, $offset=0) {
     $query = new WP_Query($arg);
 
     ?>
-    <div class="home_post_list">
+    <div class="home_post_list container">
         <?php 
         if ($query->have_posts()) {
             ?>
             <div class="row">
                 <?php
             while($query->have_posts()) {
-                $query->the_post();
+                    $query->the_post();
                  ?>
                 <div class="col-md-4 col-sm-12 col-lg-3 post_list_item">
                     <a href="<?php echo the_permalink();?>" class="post_list_link">
