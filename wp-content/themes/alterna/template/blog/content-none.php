0d7bd5ecd5ead26d2f09d6b4b637033c422d9195
pa_ku <paku@panmedia.asia>
2019-05-19 19:29:09 +0800

    修改404 ccs

--- wp-content/themes/alterna/template/blog/content-none.php
+++ wp-content/themes/alterna/template/blog/content-none.php
@@ -9,11 +9,6 @@
 html,body,.wrapper{
     height: 100%;
 }
-.footer-wrap {
-    position: absolute;
-    width: 100%;
-    bottom: 0;
-}
 </style>
 <article id="post-0" <?php post_class('entry-post');?> >
 	<h3 class="entry-title"><?php _e( 'Nothing Found', 'alterna' ); ?></h3>
