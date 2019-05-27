6204b48391d1e4e3da1caca9dbbc8338a977f0d7
pa_ku <paku@panmedia.asia>
2019-05-27 14:36:48 +0800

    fix og:img

--- wp-content/themes/alterna/header.php
+++ wp-content/themes/alterna/header.php
@@ -61,7 +61,7 @@ $current_tax = get_query_var('taxonomy');
     <meta property="og:title" content="<?php the_title();?>">
     <meta property="og:url" content="<?php echo get_permalink();?>">
     <meta property="og:description" content="<?php wp_strip_all_tags(the_excerpt());?>">
-    <meta property="og:image:url" content="<?php the_post_thumbnail_url( 'post-thumbnail' );?>">
+    <meta property="og:image:url" content="<?php the_post_thumbnail_url( 'thumbnail' );?>">
     <meta property="og:image:width" content="350">
     <meta property="og:image:height" content="350">
 <?php wp_head();?>
