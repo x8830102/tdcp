858fb6fa04853ee161a094dfe58cb2dfff96f839
pa_ku <paku@panmedia.asia>
2019-05-27 14:38:26 +0800

    fix og:img size

--- wp-content/themes/alterna/header.php
+++ wp-content/themes/alterna/header.php
@@ -62,8 +62,8 @@ $current_tax = get_query_var('taxonomy');
     <meta property="og:url" content="<?php echo get_permalink();?>">
     <meta property="og:description" content="<?php wp_strip_all_tags(the_excerpt());?>">
     <meta property="og:image:url" content="<?php the_post_thumbnail_url( 'thumbnail' );?>">
-    <meta property="og:image:width" content="350">
-    <meta property="og:image:height" content="350">
+    <meta property="og:image:width" content="300">
+    <meta property="og:image:height" content="300">
 <?php wp_head();?>
 </head>
 <body <?php body_class($global_layout); ?>>
