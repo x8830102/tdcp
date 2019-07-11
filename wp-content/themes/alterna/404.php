450cf4f4078e236c0592c16f0b55bcbf30356616
pa_ku <paku@panmedia.asia>
2019-07-11 15:15:38 +0800

    modify 404.php

--- wp-content/themes/alterna/404.php
+++ wp-content/themes/alterna/404.php
@@ -8,12 +8,12 @@ get_header();
 ?>
 <div id="main" class="container">
 	<div class="row">
-        <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3">
+        <div class="col-md-12 col-md-offset-3 col-sm-6 col-sm-offset-3">
             <div class="error-404">
             	<span class="error-icon"><i class="fa fa-exclamation-triangle"></i></span>
                 <h1 class="entry-title"><?php _e( '404 - Page not found', 'alterna' ); ?></h1>
                 <h5><?php _e( "OOPS!Looks like the page you are looking for isn't there.You may have mistyped the address or the page may have moved.", 'alterna' ); ?></h5>
-                <div class="widget_search"><?php get_search_form(); ?></div>
+                <div class="widget_search"><?php //get_search_form(); ?></div>
             </div>
         </div>
     </div>
